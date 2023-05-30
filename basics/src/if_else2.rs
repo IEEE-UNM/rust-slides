92d0fdd8021e23791095bf735f0272f7e3dc186f
ecyht2 <ecyht2@nottingham.edu.my>
2023-05-30 11:11:08 +0800

    Added if else slides

---
+++ basics/src/if_else2.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 69;
+    if x < 50 {
+        println!("x is less than 50")
+    } else if x < 100 {
+        println!("x is at least 50 but less than 100")
+    } else {
+        println!("x is greater than or equal to 100")
+    }
+}
